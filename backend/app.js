39d1548d9c5e5e1d3ab939ad34691f48a28e5916
HIMANSHUMISHRA389 <himanshumishra389@gmail.com>
2023-04-13 18:22:28 +0530

    front-end

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,13 @@
 const express=require("express");
 const app=express();
 
+//Route Imports
+const product=require("./routes/productRoute")
+
+
+
+
+app.use('/api/v1',product);
+
 
 module.exports=app;
